823ef8501a45f410cca1cc1e74317d9584ae9bbb
chanlazaro <lazarochan03@gmail.com>
2021-07-25 13:41:56 +0800

    Added change election status

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,6 +4,7 @@ namespace App\Controllers;
 use App\Models as userModels;
 use Modules\Announcements\Models as announceModels;
 use Modules\Sliders\Models as sliderModels;
+use Modules\Elections\Models as Election;
 
 class Home extends BaseController
 {
@@ -11,6 +12,35 @@ class Home extends BaseController
         $this->userModel = new userModels\UserModel();
         $this->announceModel = new announceModels\AnnouncementModel();
         $this->sliderModel = new sliderModels\SliderModel();
+        $this->electionModel = new Election\ElectionModel();
+
+        foreach($this->electionModel->findAll() as $election) {
+            if($election['status'] == 'Application') {
+                if(strtotime($election['vote_start']) <= strtotime(date('Y-m-d'))) {
+                    $data = [
+                        'id' => $election['id'],
+                        'status' => 'Voting'
+                    ];
+                    if($this->electionModel->save($data)) {
+                        // echo $election['title'].' starts now';
+                    } else {
+                        // echo $election['title'].' has an error starting';
+                    }
+                }
+            } elseif($election['status'] == 'Voting') {
+                if(strtotime($election['vote_end']) <= strtotime(date('Y-m-d'))) {
+                    $data = [
+                        'id' => $election['id'],
+                        'status' => 'Finished'
+                    ];
+                    if($this->electionModel->save($data)) {
+                        // echo $election['title'].' end now';
+                    } else {
+                        // echo $election['title'].' has an error starting';
+                    }
+                }
+            }
+        }
     }
 
 	public function index() {
