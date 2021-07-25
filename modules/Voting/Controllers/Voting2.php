dffa9e7859ba753b2f1f6d736fc495ae6dcd59cb
chanlazaro <lazarochan03@gmail.com>
2021-07-25 13:43:38 +0800

    Fixed error when getting election data

--- modules/Voting/Controllers/Voting2.php
+++ modules/Voting/Controllers/Voting2.php
@@ -72,42 +72,6 @@ class Voting2 extends BaseController
         $data['elections'] = $this->electionModel->where('status', 'Voting')->findAll();
 
         $data['positions'] = $this->positionModel->findAll();
-        // if($this->request->getMethod() === 'post') {
-        //     die();
-        //     // save first to votes table
-        //     $voter = [
-        //         'election_id' => $this->request->getVar('election_id'),
-        //         'voters_id' => $this->session->get('user_id'),
-        //     ];
-        //     if($this->voteModel->save($voter)) {
-        //         $voterData = $this->voteModel->where(['election_id' => $voter['election_id'], 'voters_id' => $this->session->get('user_id')])->first();
-        //         $data['electionPosition'] = $this->positionModel->where('election_id', $voter['election_id'])->findAll();
-        //         // pagtapos mag save ng voter detail, isasave na votes
-        //         foreach($data['electionPosition'] as $position) {
-        //             if($this->request->getVar($position['id']) != 0) {
-        //                 $voteData = [
-        //                     'votes_id' => $voterData['id'],
-        //                     'position_id' => $position['id'],
-        //                     'candidate_id' => $this->request->getVar($position['id']),
-        //                 ];
-        //             } else {
-        //                 $voteData = [
-        //                     'votes_id' => $voterData['id'],
-        //                     'position_id' => $position['id'],
-        //                     'candidate_id' => 0,
-        //                 ];
-        //             }
-        //             $this->voteDetailModel->save($voteData);
-        //         }
-        //         $this->session->setFlashdata('firstVoter', 'Vote casted.');
-        //         // return redirect()->to(base_url('admin/elections'));
-        //         return redirect()->back();
-        //     } else {
-        //         $this->session->setFlashdata('failMsg', 'Vote not casted, please try again.');
-        //         // return redirect()->to(base_url('admin/elections'));
-        //         return redirect()->back();
-        //     }
-        // }
 
         $data['user_details'] = user_details($this->session->get('user_id'));
         $data['active'] = 'voting';
@@ -125,7 +89,7 @@ class Voting2 extends BaseController
             if($this->voteModel->save($voter)) {
                 $voterData = $this->voteModel->where(['election_id' => $voter['election_id'], 'voters_id' => $this->session->get('user_id')])->first();
                 $data['electionPosition'] = $this->positionModel->where('election_id', $voter['election_id'])->findAll();
-                $election = $this->electionModel->where(['election_id' => $voter['election_id'], 'status' => 'Voting'])->first();
+                $election = $this->electionModel->where(['id' => $voter['election_id'], 'status' => 'Voting'])->first();
                 // pagtapos mag save ng voter detail, isasave na votes
                 $activityLog['user'] = $this->session->get('user_id');
                 $activityLog['description'] = 'Voted for the election: '.$election['title'];
