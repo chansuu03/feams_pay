7002ab80d5693efe648e0d99cff0bd855429777b
chanlazaro <lazarochan03@gmail.com>
2021-07-25 13:40:56 +0800

    Fix created_at date

--- app/Database/Seeds/RolePermissionSeeder.php
+++ app/Database/Seeds/RolePermissionSeeder.php
@@ -11,192 +11,230 @@ class RolePermissionSeeder extends Seeder
               [
                 'role_id' => '1',
                 'perm_mod' => 'USR',
-                'perm_id' => '1'
+                'perm_id' => '1',
+                'created_at' => date('Y-m-d H:i:s')
               ],
               [
                 'role_id' => '1',
                 'perm_mod' => 'USR',
-                'perm_id' => '2'
+                'perm_id' => '2',
+                'created_at' => date('Y-m-d H:i:s')
               ],
               [
                 'role_id' => '1',
                 'perm_mod' => 'USR',
-                'perm_id' => '3'
+                'perm_id' => '3',
+                'created_at' => date('Y-m-d H:i:s')
               ],
               [
                 'role_id' => '1',
                 'perm_mod' => 'USR',
-                'perm_id' => '4'
+                'perm_id' => '4',
+                'created_at' => date('Y-m-d H:i:s')
               ],
               [
                 'role_id' => '1',
                 'perm_mod' => 'ROLE',
-                'perm_id' => '5'
+                'perm_id' => '5',
+                'created_at' => date('Y-m-d H:i:s')
               ],
               [
                 'role_id' => '1',
                 'perm_mod' => 'ROLE',
-                'perm_id' => '6'
+                'perm_id' => '6',
+                'created_at' => date('Y-m-d H:i:s')
               ],
               [
                 'role_id' => '1',
                 'perm_mod' => 'ROLE',
-                'perm_id' => '7'
+                'perm_id' => '7',
+                'created_at' => date('Y-m-d H:i:s')
               ],
               [
                 'role_id' => '1',
                 'perm_mod' => 'ROLE',
-                'perm_id' => '8'
+                'perm_id' => '8',
+                'created_at' => date('Y-m-d H:i:s')
               ],
               [
                 'perm_mod' => 'PERM',
                 'role_id' => '1',
-                'perm_id' => '9'
+                'perm_id' => '9',
+                'created_at' => date('Y-m-d H:i:s')
               ],
               [
                 'perm_mod' => 'PERM',
                 'role_id' => '1',
-                'perm_id' => '10'
+                'perm_id' => '10',
+                'created_at' => date('Y-m-d H:i:s')
               ],
               [
                 'perm_mod' => 'ANN',
                 'role_id' => '1',
-                'perm_id' => '11'
+                'perm_id' => '11',
+                'created_at' => date('Y-m-d H:i:s')
               ],
               [
                 'perm_mod' => 'ANN',
                 'role_id' => '1',
-                'perm_id' => '12'
+                'perm_id' => '12',
+                'created_at' => date('Y-m-d H:i:s')
               ],
               [
                 'perm_mod' => 'ANN',
                 'role_id' => '1',
-                'perm_id' => '13'
+                'perm_id' => '13',
+                'created_at' => date('Y-m-d H:i:s')
               ],
               [
                 'perm_mod' => 'ANN',
                 'role_id' => '1',
-                'perm_id' => '14'
+                'perm_id' => '14',
+                'created_at' => date('Y-m-d H:i:s')
               ],
               [
                 'perm_mod' => 'SLID',
                 'role_id' => '1',
-                'perm_id' => '15'
+                'perm_id' => '15',
+                'created_at' => date('Y-m-d H:i:s')
               ],
               [
                 'perm_mod' => 'SLID',
-				        'role_id' => '1',
-				        'perm_id' => '16'
+				'role_id' => '1',
+				'perm_id' => '16',
+                'created_at' => date('Y-m-d H:i:s')
               ],
               [
                 'perm_mod' => 'SLID',
-				        'role_id' => '1',
-				        'perm_id' => '17'
+				'role_id' => '1',
+				'perm_id' => '17',
+                'created_at' => date('Y-m-d H:i:s')
               ],
               [
                 'perm_mod' => 'SLID',
                 'role_id' => '1',
-                'perm_id' => '18'
+                'perm_id' => '18',
+                'created_at' => date('Y-m-d H:i:s')
               ],
               [
                 'perm_mod' => 'ELEC',
                 'role_id' => '1',
-                'perm_id' => '19'
+                'perm_id' => '19',
+                'created_at' => date('Y-m-d H:i:s')
             ],
             [
                 'perm_mod' => 'ELEC',
 				'role_id' => '1',
-				'perm_id' => '20'
+				'perm_id' => '20',
+                'created_at' => date('Y-m-d H:i:s')
             ],
             [
                 'perm_mod' => 'ELEC',
 				'role_id' => '1',
-				'perm_id' => '21'
+				'perm_id' => '21',
+                'created_at' => date('Y-m-d H:i:s')
             ],
             [
                 'perm_mod' => 'ELEC',
 				'role_id' => '1',
-				'perm_id' => '22'
+				'perm_id' => '22',
+                'created_at' => date('Y-m-d H:i:s')
             ],
             [
                 'perm_mod' => 'POS',
 				'role_id' => '1',
-				'perm_id' => '23'
+				'perm_id' => '23',
+                'created_at' => date('Y-m-d H:i:s')
             ],
             [
                 'perm_mod' => 'POS',
 				'role_id' => '1',
-				'perm_id' => '24'
+				'perm_id' => '24',
+                'created_at' => date('Y-m-d H:i:s')
             ],
             [
                 'perm_mod' => 'POS',
 				'role_id' => '1',
-				'perm_id' => '25'
+				'perm_id' => '25',
+                'created_at' => date('Y-m-d H:i:s')
             ],
             [
                 'perm_mod' => 'POS',
 				'role_id' => '1',
-				'perm_id' => '26'
+				'perm_id' => '26',
+                'created_at' => date('Y-m-d H:i:s')
             ],
             [
                 'perm_mod' => 'CAN',
 				'role_id' => '1',
-				'perm_id' => '27'
+				'perm_id' => '27',
+                'created_at' => date('Y-m-d H:i:s')
             ],
             [
                 'perm_mod' => 'CAN',
 				'role_id' => '1',
-				'perm_id' => '28'
+				'perm_id' => '28',
+                'created_at' => date('Y-m-d H:i:s')
             ],
             [
                 'perm_mod' => 'CAN',
 				'role_id' => '1',
-				'perm_id' => '29'
+				'perm_id' => '29',
+                'created_at' => date('Y-m-d H:i:s')
             ],
             [
                 'perm_mod' => 'CAN',
 				'role_id' => '1',
-				'perm_id' => '30'
+				'perm_id' => '30',
+                'created_at' => date('Y-m-d H:i:s')
             ],
             [
                 'perm_mod' => 'FILES',
 				'role_id' => '1',
-				'perm_id' => '31'
+				'perm_id' => '31',
+                'created_at' => date('Y-m-d H:i:s')
             ],
             [
                 'perm_mod' => 'FILES',
 				'role_id' => '1',
-				'perm_id' => '32'
+				'perm_id' => '32',
+                'created_at' => date('Y-m-d H:i:s')
             ],
             [
                 'perm_mod' => 'FICAT',
 				'role_id' => '1',
-				'perm_id' => '33'
+				'perm_id' => '33',
+                'created_at' => date('Y-m-d H:i:s')
             ],
             [
                 'perm_mod' => 'FICAT',
 				'role_id' => '1',
-				'perm_id' => '34'
+				'perm_id' => '34',
+                'created_at' => date('Y-m-d H:i:s')
             ],
             [
                 'perm_mod' => 'DISC',
 				'role_id' => '1',
-				'perm_id' => '35'
+				'perm_id' => '35',
+                'created_at' => date('Y-m-d H:i:s')
             ],
             [
                 'perm_mod' => 'COMM',
 				'role_id' => '1',
-				'perm_id' => '36'
+				'perm_id' => '36',
+                'created_at' => date('Y-m-d H:i:s')
             ],
             [
                 'perm_mod' => 'REPO',
 				'role_id' => '1',
-				'perm_id' => '37'
+				'perm_id' => '37',
+                'created_at' => date('Y-m-d H:i:s')
             ],
             [
                 'perm_mod' => 'USR',
 				'role_id' => '1',
-				'perm_id' => '38'
+				'perm_id' => '38',
+                'created_at' => date('Y-m-d H:i:s')
             ],
         ];
 		$this->db->table('role_permissions')->truncate();
