434457f52263f61d72804ee4963c549c3d14077c
chanlazaro <lazarochan03@gmail.com>
2021-07-27 12:45:08 +0800

    New sidebar

--- app/Views/adminlte.php
+++ app/Views/adminlte.php
@@ -81,7 +81,7 @@
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
-          <?= view('sidebar');?>
+          <?= view('sidebar2');?>
         </ul>
       </nav>
       <!-- /.sidebar-menu -->
