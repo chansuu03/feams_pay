9e499a0b8c70fe30c49b6ea3484f43dc611fc947
chanlazaro <lazarochan03@gmail.com>
2021-07-29 00:33:53 +0800

    Fixed cast votes

--- modules/Voting/Controllers/Voting3.php
+++ modules/Voting/Controllers/Voting3.php
@@ -57,7 +57,8 @@ class Voting3 extends BaseController
             return redirect()->to(base_url());
         }
 
-        $data['elections'] = $this->electionModel->where('status !=', 'Application')->findAll();
+        $data['elections'] = $this->electionModel->findAll();
+        // $data['elections'] = $this->electionModel->where('status !=', 'Application')->findAll();
 
         $data['user_details'] = user_details($this->session->get('user_id'));
         $data['active'] = 'voting';
@@ -99,11 +100,53 @@ class Voting3 extends BaseController
                 // print_r($data['votes']);
                 return view('Modules\Voting\Views\results2', $data);
             } else {
-                return view('Modules\Voting\Views\votingSection', $data);
+                return view('Modules\Voting\Views\electoral\votingSection', $data);
             }
         }
         if(empty($data['election'])) {
             echo 'Please select an election';
+        }
+    }
+    
+    public function castVote() {
+        if($this->request->getMethod() === 'post') {
+            // save first to votes table
+            $voter = [
+                'election_id' => $this->request->getVar('election_id'),
+                'voters_id' => $this->session->get('user_id'),
+            ];
+            if($this->voteModel->save($voter)) {
+                $voterData = $this->voteModel->where(['election_id' => $voter['election_id'], 'voters_id' => $this->session->get('user_id')])->first();
+                $data['electionPosition'] = $this->positionModel->where('election_id', $voter['election_id'])->findAll();
+                $election = $this->electionModel->where(['id' => $voter['election_id'], 'status' => 'Voting'])->first();
+                // pagtapos mag save ng voter detail, isasave na votes
+                $activityLog['user'] = $this->session->get('user_id');
+                $activityLog['description'] = 'Voted for the election: '.$election['title'];
+                $this->activityLogModel->save($activityLog);
+                foreach($data['electionPosition'] as $position) {
+                    if($this->request->getVar($position['id']) != 0) {
+                        $voteData = [
+                            'votes_id' => $voterData['id'],
+                            'position_id' => $position['id'],
+                            'candidate_id' => $this->request->getVar($position['id']),
+                        ];
+                    } else {
+                        $voteData = [
+                            'votes_id' => $voterData['id'],
+                            'position_id' => $position['id'],
+                            'candidate_id' => 0,
+                        ];
+                    }
+                    $this->voteDetailModel->save($voteData);
+                }
+                $this->session->setFlashdata('firstVoter', 'Vote casted.');
+                // return redirect()->to(base_url('admin/elections'));
+                return redirect()->back();
+            } else {
+                $this->session->setFlashdata('failMsg', 'Vote not casted, please try again.');
+                // return redirect()->to(base_url('admin/elections'));
+                return redirect()->back();
+            }
         }
     }
 }
