f81753582d3dc1bad2c4aa9e4d9ce904258460b4
chanlazaro <lazarochan03@gmail.com>
2021-08-06 23:57:50 +0800

    Added function for viewing in the homepage and wrong link in breadcrumbs

--- modules/Discussions/Models/ThreadModel.php
+++ modules/Discussions/Models/ThreadModel.php
@@ -45,4 +45,22 @@ class ThreadModel extends Model {
         $this->join('users', 'threads.creator = users.id');
         return $this->paginate($paginate);
     }
+
+    public function viewAllHomepage() {
+        $this->select('threads.id, creator, subject, link, threads.created_at, threads.deleted_at, users.first_name, users.last_name, users.username, roles.role_name');
+        $this->where(['threads.deleted_at' => NULL, 'threads.visibility' => '0']);
+        $this->join('users', 'threads.creator = users.id');
+        $this->join('roles', 'threads.visibility = roles.id', 'left');
+        $this->limit(5);
+        return $this->get()->getResultArray();
+    }
+
+    public function viewRoleHomepage($role) {
+        $this->select('threads.id, creator, subject, link, threads.created_at, threads.deleted_at, users.first_name, users.last_name, users.username, roles.role_name');
+        $this->where(['threads.deleted_at' => NULL, 'threads.visibility' => $role]);
+        $this->join('users', 'threads.creator = users.id');
+        $this->join('roles', 'threads.visibility = roles.id', 'left');
+        $this->limit(5);
+        return $this->get()->getResultArray();
+    }
 }
--- modules/Discussions/Views/thread2.php
+++ modules/Discussions/Views/thread2.php
@@ -21,7 +21,7 @@
         <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
             <li class="breadcrumb-item"><a href="#">Home</a></li>
-            <li class="breadcrumb-item"><a href="<?= base_url('admin/discussions');?>">Discussions</a></li>
+            <li class="breadcrumb-item"><a href="<?= base_url('discussions');?>">Discussions</a></li>
             <li class="breadcrumb-item active"><?= esc($threadData['subject'])?></li>
         </ol>
     </div>
